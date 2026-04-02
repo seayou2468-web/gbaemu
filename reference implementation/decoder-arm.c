c2af7cefc1a947dffde13c92a972f0725493b2a4
seayou2468-web <seayou2468@gmail.com>
2026-04-02 13:30:09 +0900

    decoder-arm.c を更新

--- reference implementation/decoder-arm.c
+++ reference implementation/decoder-arm.c
@@ -1,13 +1,3 @@
-/* Copyright (c) 2013-2014 Jeffrey Pfau
- *
- * This Source Code Form is subject to the terms of the Mozilla Public
- * License, v. 2.0. If a copy of the MPL was not distributed with this
- * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
-#include <mgba/internal/arm/decoder.h>
-
-#include <mgba/internal/arm/decoder-inlines.h>
-#include <mgba/internal/arm/emitter-arm.h>
-#include <mgba/internal/arm/isa-inlines.h>
 
 #define ADDR_MODE_1_SHIFT(OP) \
 	info->op3.reg = opcode & 0x0000000F; \
