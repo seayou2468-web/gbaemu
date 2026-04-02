ec89093057804efc6fb933cab5fd22db6eb6c866
seayou2468-web <seayou2468@gmail.com>
2026-04-02 13:31:30 +0900

    isa-thumb.c を更新

--- reference implementation/isa-thumb.c
+++ reference implementation/isa-thumb.c
@@ -1,15 +1,3 @@
-/* Copyright (c) 2013-2014 Jeffrey Pfau
- *
- * This Source Code Form is subject to the terms of the Mozilla Public
- * License, v. 2.0. If a copy of the MPL was not distributed with this
- * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
-#include <mgba/internal/arm/isa-thumb.h>
-
-#include <mgba/internal/arm/isa-inlines.h>
-#include <mgba/internal/arm/emitter-thumb.h>
-
-// Instruction definitions
-// Beware pre-processor insanity
 
 #define THUMB_ADDITION_S(M, N, D) \
 	cpu->cpsr.flags = 0; \
