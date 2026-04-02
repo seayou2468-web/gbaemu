c2b7f94834fcf0deeb40a8a4cc41ce1ce9137511
seayou2468-web <seayou2468@gmail.com>
2026-04-02 13:31:15 +0900

    isa-arm.c を更新

--- reference implementation/isa-arm.c
+++ reference implementation/isa-arm.c
@@ -1,13 +1,3 @@
-/* Copyright (c) 2013-2014 Jeffrey Pfau
- *
- * This Source Code Form is subject to the terms of the Mozilla Public
- * License, v. 2.0. If a copy of the MPL was not distributed with this
- * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
-#include <mgba/internal/arm/isa-arm.h>
-
-#include <mgba/internal/arm/arm.h>
-#include <mgba/internal/arm/emitter-arm.h>
-#include <mgba/internal/arm/isa-inlines.h>
 
 #define PSR_USER_MASK   0xF0000000
 #define PSR_PRIV_MASK   0x000000CF
