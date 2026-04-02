ec93dde82e4f1684d47a61f32c0c875deb5c2638
seayou2468-web <seayou2468@gmail.com>
2026-04-02 14:21:18 +0900

    Change includes from .mm to .c files in gba_core

--- src/core/gba_core.mm
+++ src/core/gba_core.mm
@@ -1,27 +1,27 @@
 // Optional unified Objective-C++ core implementation entry point.
 //
 // Default behavior keeps this translation unit empty because the build compiles
-// files in src/core/gba_core_modules/*.mm as standalone translation units.
+// files in src/core/gba_core_modules/*.c as standalone translation units.
 //
 // If a target wants a single aggregated TU build, define
 // GBA_CORE_USE_AGGREGATED_MODULES for this file.
 
 #if defined(GBA_CORE_USE_AGGREGATED_MODULES)
 #define GBA_CORE_MODULE_COMPILED_FROM_AGGREGATE 1
-#include "./gba_core_modules/core_bootstrap.mm"
-#include "./gba_core_modules/core_reset_state.mm"
-#include "./gba_core_modules/core_save_debug.mm"
-#include "./gba_core_modules/core_backup_runtime.mm"
-#include "./gba_core_modules/cpu_helpers.mm"
-#include "./gba_core_modules/cpu_swi.mm"
-#include "./gba_core_modules/cpu_arm_execute.mm"
-#include "./gba_core_modules/cpu_thumb_run.mm"
-#include "./gba_core_modules/memory_bus.mm"
-#include "./gba_core_modules/ppu_common.mm"
-#include "./gba_core_modules/ppu_bitmap_obj.mm"
-#include "./gba_core_modules/ppu_tile_modes.mm"
-#include "./gba_core_modules/timing_dma.mm"
-#include "./gba_core_modules/apu_interrupts.mm"
-#include "./gba_core_modules/render_debug.mm"
+#include "./gba_core_modules/core_bootstrap.c"
+#include "./gba_core_modules/core_reset_state.c"
+#include "./gba_core_modules/core_save_debug.c"
+#include "./gba_core_modules/core_backup_runtime.c"
+#include "./gba_core_modules/cpu_helpers.c"
+#include "./gba_core_modules/cpu_swi.c"
+#include "./gba_core_modules/cpu_arm_execute.c"
+#include "./gba_core_modules/cpu_thumb_run.c"
+#include "./gba_core_modules/memory_bus.c"
+#include "./gba_core_modules/ppu_cocon.c"
+#include "./gba_core_modules/ppu_bitmap_obj.c"
+#include "./gba_core_modules/ppu_tile_modes.c"
+#include "./gba_core_modules/timing_dma.c"
+#include "./gba_core_modules/apu_interrupts.c"
+#include "./gba_core_modules/render_debug.c"
 #undef GBA_CORE_MODULE_COMPILED_FROM_AGGREGATE
 #endif
