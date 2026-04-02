a31c4998ca09b32969b4de291712e8f3ea64e1b6
seayou2468-web <seayou2468@gmail.com>
2026-04-02 13:30:35 +0900

    decoder-thumb.c を更新

--- reference implementation/decoder-thumb.c
+++ reference implementation/decoder-thumb.c
@@ -1,13 +1,3 @@
-/* Copyright (c) 2013-2014 Jeffrey Pfau
- *
- * This Source Code Form is subject to the terms of the Mozilla Public
- * License, v. 2.0. If a copy of the MPL was not distributed with this
- * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
-#include <mgba/internal/arm/decoder.h>
-
-#include <mgba/internal/arm/decoder-inlines.h>
-#include <mgba/internal/arm/emitter-thumb.h>
-#include <mgba/internal/arm/isa-inlines.h>
 
 #define DEFINE_THUMB_DECODER(NAME, MNEMONIC, BODY) \
 	static void _ThumbDecode ## NAME (uint16_t opcode, struct ARMInstructionInfo* info) { \
